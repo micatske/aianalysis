58adc452dcd4c09762b9098bc9a1ed7cad3ac521
yangliu <y.liu@epfl.ch>
2022-09-03 01:19:33 +0200

    tested printer

--- symbol_func_spaces.py
+++ symbol_func_spaces.py
@@ -6,6 +6,8 @@ init_printing()
 s=Symbol("s")
 p=Symbol("p")
 f=Symbol("f")
+k=Symbol("k")
+
 
 class Lp(Function):
     @classmethod
@@ -25,3 +27,21 @@ class hom_Hs(Function):
          s,Ff = self.args
          _s,_Ff = printer._print(s), printer._print(Ff)
          return r'\left \|  %s \right \|_{\dot{H}^{%s}}' % (_Ff,_s)
+     
+class inhom_Hs(Function):
+    @classmethod
+    def eval(cls, s, Ff):
+        pass
+    def _latex(self, printer):
+         s,Ff = self.args
+         _s,_Ff = printer._print(s), printer._print(Ff)
+         return r'\left \|  %s \right \|_{H^{%s}}' % (_Ff,_s)
+
+class Sobolev(Function):
+    @classmethod
+    def eval(cls, k, p, f):
+        pass
+    def _latex(self, printer):
+         k,p,f = self.args
+         _k,_p,_f = printer._print(k),printer._print(p), printer._print(f)
+         return r'\left \|  %s \right \|_{W^{%s,%s}}' % (_f,_k,_p)
