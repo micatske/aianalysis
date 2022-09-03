13b3b86688c3197d7e69630096ee603010bd4828
yangliu <y.liu@epfl.ch>
2022-09-03 13:18:19 +0200

    Sobolev Ineq

--- symbol_func_spaces.py
+++ symbol_func_spaces.py
@@ -7,7 +7,8 @@ s=Symbol("s")
 p=Symbol("p")
 f=Symbol("f")
 k=Symbol("k")
-
+alpha=Symbol("alpha")
+r=Symbol("r")
 
 class Lp(Function):
     @classmethod
@@ -37,7 +38,7 @@ class inhom_Hs(Function):
          _s,_Ff = printer._print(s), printer._print(Ff)
          return r'\left \|  %s \right \|_{H^{%s}}' % (_Ff,_s)
 
-class Sobolev(Function):
+class Sobolev_Space(Function):
     @classmethod
     def eval(cls, k, p, f):
         pass
@@ -45,3 +46,13 @@ class Sobolev(Function):
          k,p,f = self.args
          _k,_p,_f = printer._print(k),printer._print(p), printer._print(f)
          return r'\left \|  %s \right \|_{W^{%s,%s}}' % (_f,_k,_p)
+     
+class Holder_Space(Function):
+    @classmethod
+    def eval(cls, k, alpha, f):
+        pass
+    def _latex(self, printer):
+         k,alpha,f = self.args
+         _k,_alpha,_f = printer._print(k),printer._print(alpha), printer._print(f)
+         return r'\left \|  %s \right \|_{C^{%s,%s}}' % (_f,_k,_alpha)
+     
